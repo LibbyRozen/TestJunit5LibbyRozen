72d9a1731d6d2ad73357db52430901e3713c74c3
LibbyRozen <libby.stu35@gmail.com>
2025-05-18 17:07:16 +0300

    update

--- JunitTest/src/test/java/CalculateExtension.java
+++ JunitTest/src/test/java/CalculateExtension.java
@@ -1,17 +1,21 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+@Slf4j
 public class CalculateExtension implements BeforeAllCallback, AfterAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         System.out.println("log initialized before all tests");
+        log.info("log initialized before all tests");
     }
 
     @Override
     public void afterAll(ExtensionContext extensionContext) throws Exception {
         System.out.println("log cleaned after all tests");
+        log.info("log cleaned after all tests");
     }
 
 
